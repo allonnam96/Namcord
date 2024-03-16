f87ab40cf83b4c2530e68638560707f1ef434cf7
Allon Nam <allon.nam96@gmail.com>
2024-03-15 23:05:07 -0400

    server settings

--- hooks/use-modal-store.ts
+++ hooks/use-modal-store.ts
@@ -1,7 +1,7 @@
 import { Server } from "@prisma/client";
 import { create } from "zustand";
 
-export type ModalType = "createServer" | "invite" | "leaveServer";
+export type ModalType = "createServer" | "invite" | "leaveServer" | "editServer";
 
 interface ModalData {
   server?: Server;
