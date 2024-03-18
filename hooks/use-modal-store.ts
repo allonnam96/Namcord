d59850326c627c2aa7ad348cf575ef88d9fa9260
Allon Nam <allon.nam96@gmail.com>
2024-03-18 19:14:25 -0400

    mobile toggle

--- hooks/use-modal-store.ts
+++ hooks/use-modal-store.ts
@@ -1,7 +1,7 @@
 import { Channel, ChannelType, Server } from "@prisma/client";
 import { create } from "zustand";
 
-export type ModalType = "createServer" | "invite" | "leaveServer" | "editServer" | "members" | "createChannel" | "leaveServer" | "deleteServer" | "editChannel" | "deleteChannel";
+export type ModalType = "createServer" | "invite" | "leaveServer" | "editServer" | "members" | "createChannel" | "leaveServer" | "deleteServer" | "editChannel" | "deleteChannel" | "editChannel";
 
 interface ModalData {
   server?: Server;
