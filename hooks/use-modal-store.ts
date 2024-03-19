3bc9c6f7f475fa17c7fbca8dcb61880904da9ae9
Allon Nam <allon.nam96@gmail.com>
2024-03-19 12:45:54 -0400

    emoji bar

--- hooks/use-modal-store.ts
+++ hooks/use-modal-store.ts
@@ -1,12 +1,14 @@
 import { Channel, ChannelType, Server } from "@prisma/client";
 import { create } from "zustand";
 
-export type ModalType = "createServer" | "invite" | "leaveServer" | "editServer" | "members" | "createChannel" | "leaveServer" | "deleteServer" | "editChannel" | "deleteChannel" | "editChannel";
+export type ModalType = "createServer" | "invite" | "leaveServer" | "editServer" | "members" | "createChannel" | "leaveServer" | "deleteServer" | "editChannel" | "deleteChannel" | "editChannel" | "messageFile";
 
 interface ModalData {
   server?: Server;
   channel?: Channel;
   channelType?: ChannelType;
+  apiUrl?: string;
+  query?: Record<string, any>
 }
 
 interface ModalStore {
